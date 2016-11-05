3e98cd59909e400bfb10c50080e356f4afa1acd3
Vasco Gomes <ovascogomes@gmail.com>
2016-11-05 15:18:54 +0000

    Finished main work on HighScore scene

--- Assets/Scripts/UI/Highscore/HighscoreController.cs
+++ Assets/Scripts/UI/Highscore/HighscoreController.cs
@@ -1,24 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using InControl;
 
-public class HighscoreController : MonoBehaviour {
+public class HighscoreController : MonoBehaviour
+{
+    [SerializeField] private Text[] scores;
+    [SerializeField] private Text[] names;
+    [SerializeField] private float changeSceneDelay = 15f;
 
-	void Start ()
+    private float changeSceneTime;
+
+    void Start ()
     {
-        HighScoreManager.Instance.AddScore(0, "-");
-        HighScoreManager.Instance.AddScore(0, "-");
-        HighScoreManager.Instance.AddScore(0, "-");
-        HighScoreManager.Instance.AddScore(0, "-");
-        HighScoreManager.Instance.AddScore(0, "-");
-        HighScoreManager.Instance.AddScore(0, "-");
-        HighScoreManager.Instance.AddScore(0, "-");
-        HighScoreManager.Instance.AddScore(0, "-");
-        HighScoreManager.Instance.AddScore(0, "-");
-        HighScoreManager.Instance.AddScore(0, "-");
+        changeSceneTime = Time.time + changeSceneDelay;
+
         List<HighScoreManager.HighScoreEntry> highscores = HighScoreManager.Instance.HighScores;
-        print(highscores.Count);
+        
+        for (int i = 0; i < scores.Length; i++)
+        {
+            scores[i].text = highscores[i].score.ToString();
+            names[i].text = highscores[i].name;
+        }
 	}
-	
-    
+
+    void Update()
+    {
+        if (Time.time > changeSceneTime)
+        {
+            SceneManager.LoadScene("Game");
+        }
+
+        foreach(InputDevice device in InputManager.Devices)
+        {
+            if (device.Action1.WasPressed)
+            {
+                SceneManager.LoadScene("Game");
+            }
+        }
+    }
 }
