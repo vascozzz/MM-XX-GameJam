90e0a3a1d5d9722f19cf7edc57d0f585c0e2b339
Xkynar <xkynar@gmail.com>
2016-11-05 14:39:29 +0000

    Fixed font and started highscore scene

---
+++ Assets/Scripts/UI/Highscore/BarsController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class BarsController : MonoBehaviour {
+
+    [SerializeField] private float offset = 100f;
+    [SerializeField] private float duration = 1f;
+
+    private RectTransform rectTransf;
+
+	void Start ()
+    {
+        rectTransf = GetComponent<RectTransform>();
+
+        Sequence seq = DOTween.Sequence();
+        seq.Append(rectTransf.DOAnchorPosY(offset, duration).SetEase(Ease.Linear));
+        seq.Append(rectTransf.DOAnchorPosY(-offset, duration).SetEase(Ease.Linear));
+        seq.SetLoops(-1);
+    }
+}
