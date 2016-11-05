d8a764b8519577104414d5110e2d4948bf37616d
Vasco Gomes <ovascogomes@gmail.com>
2016-11-05 15:32:32 +0000

    Added base setup for menu, instructions scenes

---
+++ Assets/Scripts/GameManagers/SkipSceneController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using InControl;
+
+public class SkipSceneController : MonoBehaviour
+{
+    [SerializeField] private string nextSceneName;
+    [SerializeField] private float changeSceneDelay = 15f;
+
+    private float changeSceneTime;
+
+    void Start()
+    {
+        changeSceneTime = Time.time + changeSceneDelay;
+    }
+
+    void Update()
+    {
+        if (Time.time > changeSceneTime)
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+
+        foreach (InputDevice device in InputManager.Devices)
+        {
+            if (device.Action1.WasPressed)
+            {
+                SceneManager.LoadScene(nextSceneName);
+            }
+        }
+    }
+}
--- Assets/Scripts/UI/Highscore/HighscoreController.cs
+++ Assets/Scripts/UI/Highscore/HighscoreController.cs
@@ -9,14 +9,9 @@ public class HighscoreController : MonoBehaviour
 {
     [SerializeField] private Text[] scores;
     [SerializeField] private Text[] names;
-    [SerializeField] private float changeSceneDelay = 15f;
-
-    private float changeSceneTime;
 
     void Start ()
     {
-        changeSceneTime = Time.time + changeSceneDelay;
-
         List<HighScoreManager.HighScoreEntry> highscores = HighScoreManager.Instance.HighScores;
         
         for (int i = 0; i < scores.Length; i++)
@@ -25,20 +20,4 @@ public class HighscoreController : MonoBehaviour
             names[i].text = highscores[i].name;
         }
 	}
-
-    void Update()
-    {
-        if (Time.time > changeSceneTime)
-        {
-            SceneManager.LoadScene("Game");
-        }
-
-        foreach(InputDevice device in InputManager.Devices)
-        {
-            if (device.Action1.WasPressed)
-            {
-                SceneManager.LoadScene("Game");
-            }
-        }
-    }
 }
