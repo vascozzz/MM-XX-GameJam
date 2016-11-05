fcd70191b70bdfb84e068f6a7d66e07ddead0e6a
Xkynar <xkynar@gmail.com>
2016-11-05 16:15:24 +0000

    Added intro scene, video

---
+++ Assets/Scripts/IntroVideoController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class IntroVideoController : MonoBehaviour {
+
+    private MovieTexture movieTexture;
+
+    void Start()
+    {
+        movieTexture = ((MovieTexture)GetComponent<RawImage>().material.mainTexture);
+        StartCoroutine(DestroyVideo(movieTexture.duration));
+        movieTexture.Play();
+    }
+
+    IEnumerator DestroyVideo(float time)
+    {
+        yield return new WaitForSeconds(time - 0.5f);
+
+        Destroy(this.gameObject);
+    }
+}
